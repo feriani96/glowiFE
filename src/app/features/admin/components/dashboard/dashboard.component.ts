4a47b52990c40e2ab278da2b81c646ba97204f8c
feriani96 <ferianiyasmine3@gmail.com>
2024-10-18 08:35:08 +0200

    fix: Refactor dashboard component using Angular Material and display product images

--- src/app/features/admin/components/dashboard/dashboard.component.ts
+++ src/app/features/admin/components/dashboard/dashboard.component.ts
@@ -9,7 +9,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class DashboardComponent implements OnInit {
   products: any = [];
-  selectedImage: string = ''; // Image sélectionnée
+  // Cette variable stocke l'image principale sélectionnée pour chaque produit
+  selectedImages: { [key: string]: number } = {};
+  private imageRotationInterval: any;
 
   constructor(
     private adminService: AdminService,
@@ -20,11 +22,19 @@ export class DashboardComponent implements OnInit {
     this.getProducts();
   }
 
+  // Récupération des produits depuis l'API
   getProducts(): void {
     this.adminService.getAllProducts().subscribe(
       (res) => {
-        this.products = res; // Stocke les produits dans la variable
+        console.log('Réponse de l\'API:', res);
+        this.products = res; 
         console.log('Produits récupérés:', this.products);
+  
+        this.products.forEach((product: any) => {
+          console.log('Produit:', product);
+          // Initialiser l'image par défaut pour chaque produit
+          this.selectedImages[product.id] = 0;
+        });
       },
       (error) => {
         console.error('Erreur de récupération des produits:', error);
@@ -33,5 +43,28 @@ export class DashboardComponent implements OnInit {
         });
       }
     );
+  }
+
+  // Fonction pour obtenir l'URL de l'image actuelle
+  currentImageUrl(product: any): string {
+    return product.imageUrls[this.selectedImages[product.id] || 0];
+  }
+
+  // Démarrer la rotation des images au survol
+  startImageRotation(product: any): void {
+    // Réinitialiser l'index d'image pour chaque produit
+    if (!this.selectedImages[product.id]) {
+      this.selectedImages[product.id] = 0;
+    }
+
+    // Rotation des images toutes les secondes
+    this.imageRotationInterval = setInterval(() => {
+      this.selectedImages[product.id] = (this.selectedImages[product.id] + 1) % product.imageUrls.length;
+    }, 1000);
+  }
+
+  // Arrêter la rotation des images lorsque la souris quitte le produit
+  stopImageRotation(): void {
+    clearInterval(this.imageRotationInterval);
   }
 }
